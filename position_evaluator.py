ca25e2ce31a9976ce03c100980ed4c24581b4c2e
KalinKrastev313 <93783199+KalinKrastev313@users.noreply.github.com>
2024-01-14 14:50:56 +0200

    Space advantage calculation added. Mate value infinity bug fixed

--- position_evaluator.py
+++ position_evaluator.py
@@ -1,25 +1,38 @@
 import chess
 
 positional_values_dict = {
-    'possible_move': 0.1
+    'possible_move': 0.1,
+    'one_square_space': 1
 }
 
 
 def total_possible_moves_advantage_evaluation(board: chess.Board):
-    test_board = board.copy()
-    side_to_move = test_board.turn
-    number_of_possible_moves = len(list(test_board.legal_moves))
-    test_board.push(chess.Move.null())
-    other_side_possible_moves = len(list(test_board.legal_moves))
+    side_to_move = board.turn
+    number_of_possible_moves = len(list(board.legal_moves))
+    board.push(chess.Move.null())
+    other_side_possible_moves = len(list(board.legal_moves))
     evaluation = (number_of_possible_moves - other_side_possible_moves) * positional_values_dict['possible_move']
     if side_to_move:
         return evaluation
     return -evaluation
 
 
+def pieces_are_forward(board: chess.Board):
+    evaluation = 0
+    for square in chess.SQUARES:
+        piece = board.piece_at(square)
+        if piece is not None:
+            if piece.color:
+                evaluation += chess.square_rank(square)
+            else:
+                evaluation += chess.square_rank(square) - 7
+
+    return evaluation * positional_values_dict['one_square_space']
+
+
 positional_evaluation_functions_mapping = {
     'amount_of_possible_moves': total_possible_moves_advantage_evaluation,
-    # 'pieces_are_forward': 5,
+    'space_advantage': pieces_are_forward
     # 'pieces_in_center': 5,
     # 'has_knight': 5,
     # 'has_bishop': 5,
@@ -58,7 +71,7 @@ class PositionalEvaluator:
     def get_positional_evaluation(self, board: chess.Board):
         evaluation = 0
         for func in self.POSITIONAL_EVALUATION_FUNCS_MAPPING:
-            evaluation += self.POSITIONAL_EVALUATION_FUNCS_MAPPING[func](board)
+            evaluation += self.POSITIONAL_EVALUATION_FUNCS_MAPPING[func](board.copy())
 
         return evaluation
 
@@ -82,9 +95,9 @@ class PositionEvaluator:
         # white is True, black is False, None is draw
         winner = board.outcome().winner
         if winner is True:
-            return float('inf')
+            return float(2000)
         if winner is False:
-            return float('-inf')
+            return float(-2000)
         if winner is None:
             return float(0)
 
