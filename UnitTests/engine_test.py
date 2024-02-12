26baac5b3a32c8948248f2ab9f183e3d08f8dff5
KalinKrastev313 <93783199+KalinKrastev313@users.noreply.github.com>
2024-02-12 20:30:42 +0200

    engine unittests

--- UnitTests/engine_test.py
+++ UnitTests/engine_test.py
@@ -6,24 +6,45 @@ from unittest import TestCase
 
 
 class EngineTest(TestCase):
+    def setUp(self):
+        self.engine = Engine()
+        self.engine.MAX_DEPTH = 1
+
     def test_create_evaluator(self):
         board = chess.Board(fen='2b1k2r/1p1n3p/6p1/1P1QPp2/8/2P5/1b1P1KPP/7r b k - 0 21')
-        engine = Engine()
 
-        evaluator = engine.create_evaluator(board)
+        evaluator = self.engine.create_evaluator(board)
 
         self.assertEquals(evaluator.best_move, None)
         self.assertEquals(evaluator.alpha, float('-inf'))
         self.assertEquals(evaluator.beta, float('inf'))
-        self.assertEquals(evaluator.depth, engine.MAX_DEPTH)
+        self.assertEquals(evaluator.depth, self.engine.MAX_DEPTH)
         self.assertEquals(evaluator.board, board)
 
     def test_suggest_move_executes_min_max_algorithm(self):
-        engine = Engine()
         with patch.object(MinMaxEvaluator, 'min_max') as min_max:
-            engine.suggest_move(chess.Board(fen='2b1k2r/1p1n3p/6p1/1P1QPp2/8/2P5/1b1P1KPP/7r b k - 0 21'))
+            self.engine.suggest_move(chess.Board(fen='2b1k2r/1p1n3p/6p1/1P1QPp2/8/2P5/1b1P1KPP/7r b k - 0 21'))
 
         self.assertEquals(min_max.call_count, True)
+
+    def test_suggest_move_returns_a_move(self):
+        board = chess.Board()
+
+        suggested_move = self.engine.suggest_move(board)
+        self.assertIsInstance(suggested_move, chess.Move)
+
+    def test_suggest_move_finds_mate_in_one(self):
+        board = chess.Board(fen='3k4/8/3K4/5R2/8/8/8/8 w - - 0 1')
+
+        suggested_move = self.engine.suggest_move(board)
+        self.assertEquals(suggested_move, chess.Move.from_uci('f5f8'))
+
+    def test_suggest_move_finds_mate_in_two(self):
+        board = chess.Board(fen='4k3/8/3K4/6R1/8/8/8/8 w - - 0 1')
+        self.engine.MAX_DEPTH = 3
+
+        suggested_move = self.engine.suggest_move(board)
+        self.assertEquals(suggested_move, chess.Move.from_uci('g5f5'))
 
 
 class MinMaxEvaluatorTest(TestCase):
